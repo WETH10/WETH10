f335ee24a9ee0b8cb17504f033746c098f009ec7
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-10-09 22:15:26 +0100

    feat: added basic tests for deposit and withdraw

--- test/01_WETH2.test.js
+++ test/01_WETH2.test.js
@@ -1,9 +1,10 @@
 const WETH2 = artifacts.require('WETH2')
 
+const { BN, expectRevert } = require('@openzeppelin/test-helpers')
 require('chai').use(require('chai-as-promised')).should()
 
 contract('TestOracle', (accounts) => {
-  const [deployer] = accounts
+  const [deployer, user] = accounts
   let weth
 
   beforeEach(async () => {
@@ -14,6 +15,26 @@ contract('TestOracle', (accounts) => {
     it('returns the name', async () => {
       let name = await weth.name()
       name.should.equal('Wrapped Ether')
+    })
+
+    it('deposits ether', async () => {
+      const balanceBefore = await weth.balanceOf(user)
+      await weth.deposit({ from: user, value: 1 })
+      const balanceAfter = await weth.balanceOf(user)
+      balanceAfter.toString().should.equal(balanceBefore.add(new BN('1')).toString())
+    })
+
+    describe('with a positive balance', async () => {
+      beforeEach(async () => {
+        await weth.deposit({ from: user, value: 1 })
+      })
+
+      it('withdraws ether', async () => {
+        const balanceBefore = await weth.balanceOf(user)
+        await weth.withdraw(1, { from: user })
+        const balanceAfter = await weth.balanceOf(user)
+        balanceAfter.toString().should.equal(balanceBefore.sub(new BN('1')).toString())
+      })
     })
   })
 })
