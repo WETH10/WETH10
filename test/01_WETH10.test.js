c663c10d7f6504a63b2e09336af87b5c16466f6f
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-10-09 22:30:42 +0100

    feat: basic tests for ERC20 functions

--- test/01_WETH10.test.js
+++ test/01_WETH10.test.js
@@ -4,7 +4,7 @@ const { BN, expectRevert } = require('@openzeppelin/test-helpers')
 require('chai').use(require('chai-as-promised')).should()
 
 contract('TestOracle', (accounts) => {
-  const [deployer, user] = accounts
+  const [deployer, user1, user2] = accounts
   let weth
 
   beforeEach(async () => {
@@ -18,22 +18,56 @@ contract('TestOracle', (accounts) => {
     })
 
     it('deposits ether', async () => {
-      const balanceBefore = await weth.balanceOf(user)
-      await weth.deposit({ from: user, value: 1 })
-      const balanceAfter = await weth.balanceOf(user)
+      const balanceBefore = await weth.balanceOf(user1)
+      await weth.deposit({ from: user1, value: 1 })
+      const balanceAfter = await weth.balanceOf(user1)
       balanceAfter.toString().should.equal(balanceBefore.add(new BN('1')).toString())
     })
 
     describe('with a positive balance', async () => {
       beforeEach(async () => {
-        await weth.deposit({ from: user, value: 1 })
+        await weth.deposit({ from: user1, value: 1 })
       })
 
       it('withdraws ether', async () => {
-        const balanceBefore = await weth.balanceOf(user)
-        await weth.withdraw(1, { from: user })
-        const balanceAfter = await weth.balanceOf(user)
+        const balanceBefore = await weth.balanceOf(user1)
+        await weth.withdraw(1, { from: user1 })
+        const balanceAfter = await weth.balanceOf(user1)
         balanceAfter.toString().should.equal(balanceBefore.sub(new BN('1')).toString())
+      })
+
+      it('transfers ether', async () => {
+        const balanceBefore = await weth.balanceOf(user2)
+        await weth.transfer(user2, 1, { from: user1 })
+        const balanceAfter = await weth.balanceOf(user2)
+        balanceAfter.toString().should.equal(balanceBefore.add(new BN('1')).toString())
+      })
+
+      it('transfers ether using transferFrom', async () => {
+        const balanceBefore = await weth.balanceOf(user2)
+        await weth.transferFrom(user1, user2, 1, { from: user1 })
+        const balanceAfter = await weth.balanceOf(user2)
+        balanceAfter.toString().should.equal(balanceBefore.add(new BN('1')).toString())
+      })
+
+      it('approves to increase allowance', async () => {
+        const allowanceBefore = await weth.allowance(user1, user2)
+        await weth.approve(user2, 1, { from: user1 })
+        const allowanceAfter = await weth.allowance(user1, user2)
+        allowanceAfter.toString().should.equal(allowanceBefore.add(new BN('1')).toString())
+      })
+
+      describe('with a positive balance', async () => {
+        beforeEach(async () => {
+          await weth.approve(user2, 1, { from: user1 })
+        })
+
+        it('transfers ether using transferFrom and allowance', async () => {
+          const balanceBefore = await weth.balanceOf(user2)
+          await weth.transferFrom(user1, user2, 1, { from: user2 })
+          const balanceAfter = await weth.balanceOf(user2)
+          balanceAfter.toString().should.equal(balanceBefore.add(new BN('1')).toString())
+        })
       })
     })
   })
