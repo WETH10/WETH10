2ec96e9895f86af2628badcc9b8f0b14fe136aa1
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-11-10 10:36:42 +0000

    network: kovan deploy

--- migrations/1_deploy.js
+++ migrations/1_deploy.js
@@ -1,6 +1,7 @@
 const WETH10 = artifacts.require('WETH10')
 
 module.exports = async function (deployer, network) {
-  await deployer.deploy(WETH10)
-  weth = await WETH10.deployed()
+  if (network === 'kovan') {
+    await deployer.deploy(WETH10, '0xd0A1E359811322d97991E03f863a0C30C2cF029C')  
+  }
 }
