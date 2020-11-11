6418da04432ce08fac568513010ab14881330f11
Wighawag <wighawag@gmail.com>
2020-11-11 16:17:01 +0000

    determinsitc deployment via buidler-deploy

--- buidler.config.js
+++ buidler.config.js
@@ -1,11 +1,54 @@
+const fs = require('fs')
+const path = require('path')
 usePlugin('@nomiclabs/buidler-truffle5')
 usePlugin('solidity-coverage')
 usePlugin('buidler-gas-reporter')
+usePlugin('buidler-deploy')
+
+
+function nodeUrl(network) {
+  let infuraKey
+  try {
+    infuraKey = fs.readFileSync(path.resolve(__dirname, '.infuraKey')).toString().trim()
+  } catch(e) {
+    infuraKey = ''
+  }
+  return `https://${network}.infura.io/v3/${infuraKey}`
+}
+
+let mnemonic = process.env.MNEMONIC;
+if (!mnemonic) {
+  try {
+    mnemonic = fs.readFileSync(path.resolve(__dirname, '.secret')).toString().trim()
+  } catch(e){}
+}
+const accounts = mnemonic ? {
+  mnemonic,
+}: undefined;
 
 module.exports = {
   defaultNetwork: 'buidlerevm',
   networks: {
-    buidlerevm: {},
+    kovan: {
+      accounts,
+      url: nodeUrl('kovan')
+    },
+    goerli: {
+      accounts,
+      url: nodeUrl('goerli'),
+    },
+    rinkeby: {
+      accounts,
+      url: nodeUrl('rinkeby')
+    },
+    ropsten: {
+      accounts,
+      url: nodeUrl('ropsten')
+    },
+    mainnet: {
+      accounts,
+      url: nodeUrl('mainnet')
+    },
     coverage: {
       url: 'http://127.0.0.1:8555',
     },
@@ -28,4 +71,7 @@ module.exports = {
     coverageJson: './coverage.json',
     artifacts: './artifacts',
   },
+  namedAccounts: {
+    deployer: 0
+  }
 }
--- migrations/1_deploy.js
+++
@@ -1,7 +0,0 @@
-const WETH10 = artifacts.require('WETH10')
-
-module.exports = async function (deployer, network) {
-  if (network === 'kovan') {
-    await deployer.deploy(WETH10, '0xd0A1E359811322d97991E03f863a0C30C2cF029C')  
-  }
-}
