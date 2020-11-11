e8799aa5febddbe7f7605560cbe99ee545acd64a
Wighawag <wighawag@gmail.com>
2020-11-11 16:39:39 +0000

    add metadta to deployments

--- buidler.config.js
+++ buidler.config.js
@@ -6,6 +6,17 @@ usePlugin('buidler-gas-reporter')
 usePlugin('buidler-deploy')
 
 
+// REQUIRED TO ENSURE METADATA IS SAVED IN DEPLOYMENTS (because solidity-coverage disable it otherwise)
+const {
+  TASK_COMPILE_GET_COMPILER_INPUT
+} = require("@nomiclabs/buidler/builtin-tasks/task-names");
+task(TASK_COMPILE_GET_COMPILER_INPUT).setAction(async (_, bre, runSuper) => {
+  const input = await runSuper();
+  input.settings.metadata.useLiteralContent = bre.network.name !== "coverage";
+  return input;
+})
+
+
 function nodeUrl(network) {
   let infuraKey
   try {
