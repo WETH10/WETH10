ea7e6e45cb6936d5235fc0c7c15914e69811a2eb
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-02-28 13:21:01 -0500

    Update IERC2612.sol
    change `amount` variable to `value` to reflect language of EIP and OZ implementation, see:
    
    https://eips.ethereum.org/EIPS/eip-2612
    
    https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/draft-IERC20Permit.sol

--- contracts/interfaces/IERC2612.sol
+++ contracts/interfaces/IERC2612.sol
@@ -13,7 +13,7 @@ pragma solidity ^0.7.6;
  */
 interface IERC2612 {
     /**
-     * @dev Sets `amount` as the allowance of `spender` over `owner`'s tokens,
+     * @dev Sets `value` as the allowance of `spender` over `owner`'s tokens,
      * given `owner`'s signed approval.
      *
      * IMPORTANT: The same issues {IERC20-approve} has related to transaction
@@ -28,19 +28,19 @@ interface IERC2612 {
      * - `deadline` must be a timestamp in the future.
      * - `v`, `r` and `s` must be a valid `secp256k1` signature from `owner`
      * over the EIP712-formatted function arguments.
-     * - the signature must use ``owner``'s current nonce (see {nonces}).
+     * - the signature must use `owner`'s current nonce (see {nonces}).
      *
      * For more information on the signature format, see the
      * https://eips.ethereum.org/EIPS/eip-2612#specification[relevant EIP
      * section].
      */
-    function permit(address owner, address spender, uint256 amount, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external;
+    function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external;
 
     /**
      * @dev Returns the current ERC2612 nonce for `owner`. This value must be
      * included whenever a signature is generated for {permit}.
      *
-     * Every successful call to {permit} increases ``owner``'s nonce by one. This
+     * Every successful call to {permit} increases `owner`'s nonce by one. This
      * prevents a signature from being used multiple times.
      */
     function nonces(address owner) external view returns (uint256);
